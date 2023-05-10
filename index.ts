1fb1d0bec06ca461b310fd9b3e6dc6bbcc58dee0
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2023-05-10 21:42:47 +0700

    add service account to deployment

--- index.ts
+++ index.ts
@@ -13,6 +13,51 @@ const cluster = new eks.Cluster('cluster', {
   desiredCapacity: 2,
   minSize: 1,
   maxSize: 2,
+  createOidcProvider: true,
+})
+
+// Define the service account name
+const saName = 'secret-manager-sa'
+
+// Get the OIDC provider's URL for the cluster.
+export const clusterOidcProvider = cluster?.core?.oidcProvider?.url
+
+// Create a policy that allows the cluster to access the AWS Secret Manager.
+const policy = new aws.iam.Policy('secret-manager-policy', {
+  policy: JSON.stringify({
+    Version: '2012-10-17',
+    Statement: [
+      {
+        Effect: 'Allow',
+        Action: ['secretsmanager:GetSecretValue', 'secretsmanager:DescribeSecret'],
+        Resource: ['arn:*:secretsmanager:*'],
+      },
+    ],
+  }),
+})
+
+// Create a new IAM role that assumes the AssumeRoleWebWebIdentity policy
+const saRole = new aws.iam.Role(saName, {
+  assumeRolePolicy: {
+    Version: '2012-10-17',
+    Statement: [
+      {
+        Effect: 'Allow',
+        Principal: {
+          Service: 'eks.amazonaws.com',
+        },
+        Action: ['sts:AssumeRole'],
+      },
+    ],
+  },
+})
+
+export const saRoleArn = saRole.arn
+
+// Attach the IAM role to an AWS Secret Manager access policy.
+new aws.iam.RolePolicyAttachment(saName, {
+  policyArn: policy.arn,
+  role: saRole,
 })
 
 // Export the cluster's kubeconfig.
